2b3c9430ba9c17b4d4ca8446f06d6e6d40635c69
paulinelegat40 <118973794+paulinelegat40@users.noreply.github.com>
2023-01-05 21:38:37 +0100

    Update fmu_simulation.py

--- fmu_simulation.py
+++ fmu_simulation.py
@@ -1,7 +1,7 @@
 # Test avec valeurs prédéfinies comme si on avait rempli les tableaux avec les données FMU
-V2 = [26,25,24,27,25.8,25.2,24.8,24.2,20,32,26.9,27.9,25.4,26.7,25.1,24.9,24.6,24.3,24.1,21,18]
-C2 = [0 , 0.2 , 0, 0.1, 0, 0.3, 0, 0.3, 0, 0.1, 0, 0.4 ,0, 0.2 ,0,0.1,0,0.3,0,0.4,0]
-D2 = [0,0,0,100,100,100,100,100,100, 200,200, 200, 200,200,200,200,200,200,200,200,200]
+V = [26,25,24,27,25.8,25.2,24.8,24.2,20,32,26.9,27.9,25.4,26.7,25.1,24.9,24.6,24.3,24.1,21,18]
+C = [0 , 0.2 , 0, 0.1, 0, 0.3, 0, 0.3, 0, 0.1, 0, 0.4 ,0, 0.2 ,0,0.1,0,0.3,0,0.4,0]
+D = [0,0,0,100,100,100,100,100,100, 200,200, 200, 200,200,200,200,200,200,200,200,200]
 # generation du graphe
 
 import networkx as nx
@@ -16,22 +16,27 @@ m = 3
 l= 1
 k=1
 
-# Ajout manuel du 1er noeud
+# 1st node added manually (level -1) and named "B"
 G.add_node('B',level = -1)
 
-G.add_node(V2[0],level = 0)
-G.add_node(V2[1],level = 0)
-G.add_node(V2[2],level = 0)
+# 3 starting speeds added given the 3 first values of V2 vector (level 0)
+G.add_node(V[0],level = 0)
+G.add_node(V[1],level = 0)
+G.add_node(V[2],level = 0)
+
 i=0
 
 for k in range (3,len(V2)):
     if k == m*2+3:
         l+=1
         m = k
-    G.add_node(V2[k], level=l)
+    G.add_node(V[k], level=l)
 
-# Ajout manuel du dernier noeud
+# Last node added manually (level L+1 = last level) and named "E"
 G.add_node('E',level = l+1)
+
+
+
 # layout des noeuds
 pos = nx.multipartite_layout(G, subset_key="level")
 
@@ -42,7 +47,7 @@ m = 3
 
 k=0
 for k in range (0, len(V2)//2-1):
-    E = E + [(V2[k],V2[m],C2[m]), (V2[k],V2[m+1],C2[m+1])]
+    E = E + [(V[k],V[m],C[m]), (V[k],V[m+1],C[m+1])]
     m+=2
     k+=1
 
